624f50bf4c7e4e1c25bc9da4ac637b44b31c6af4
Drew Chase <dcmanproductions@gmail.com>
2020-07-03 15:03:23 -0400

    Added Scaffolding

---
+++ MDM Proxy/ProxyManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.drewchaseproject.MDM.Library.Proxy
+{
+    public class ProxyManager
+    {
+        public void Test()
+        {
+            string urlAddress = "http://google.com";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                    readStream = new StreamReader(receiveStream);
+                else
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+
+                string data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+        }
+    }
+}
