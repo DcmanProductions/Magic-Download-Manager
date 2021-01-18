c9fff044686e056a331558cd0d0a8555d3a83a5b
Drew Chase <dcmanproductions@gmail.com>
2021-01-18 01:26:55 -0500

    Worked on Download Section

--- Magic Download Manager/assets/js/download.js
+++ Magic Download Manager/assets/js/download.js
@@ -1,3 +1,58 @@
-class Download {
-    
+var dlQueue = new Array();
+var autoClear = false;
+
+function AddDownload(dlFile) {
+    dlQueue.push(dlFile);
+    document.getElementById('download-list').innerHTML += dlFile.HTML;
 }
+
+function ClearDownloads() {
+    document.querySelectorAll('.download-item-completed').forEach(item => {
+        item.parentElement.removeChild(item);
+    })
+}
+
+function ToggleAutoClear() {
+    let cl = document.getElementById('clear-download-btn');
+    cl.classList.toggle('download-list-controls-toggled');
+    autoClear = cl.classList.contains('download-list-controls-toggled');
+    cl.innerHTML = autoClear ? "AUTO" : "CLEAR";
+}
+
+function UpdateDownloadQueue() {
+    let dList = document.getElementById('download-list');
+    dList.innerHTML = "";
+    dlQueue.forEach(n => {
+        dList.innerHTML += n.HTML;
+        console.log(n.HTML)
+    })
+}
+
+setInterval(() => {
+    let cl = document.getElementById('clear-download-btn');
+    cl.innerHTML = autoClear ? "AUTO" : "CLEAR";
+    let downloadItems = document.querySelectorAll('.download-item');
+    downloadItems.forEach(item => {
+        let progress_bar = item.querySelector('.download-actual-progress');
+        let progress = item.querySelector('.download-progress-done');
+        let value = Math.floor(progress_bar.value * 100);
+        if (value >= 3)
+            progress.style.opacity = 1;
+        if (value >= 99) {
+            progress.innerHTML = `Completed!`;
+            progress.style.width = `100%`;
+            progress_bar.value += 1;
+            item.classList.add('download-item-completed');
+            item.classList.remove('download-item');
+            if (autoClear)
+                setTimeout(() => {
+                    item.parentElement.removeChild(item);
+                }, 1500)
+        } else {
+            progress_bar.value += .1;
+            progress.style.width = `${value}%`;
+            progress.innerHTML = `${value}%`;
+        }
+    })
+
+}, 700)
--- Magic Download Manager/assets/js/downloadFile.js
+++ Magic Download Manager/assets/js/downloadFile.js
@@ -27,4 +27,31 @@ class DownloadFile {
             console.log(l);
         })
     }
+
+    get HTML() {
+        return `
+        <div class="download-item">
+            <div class="download-title">
+                test
+            </div>
+            <div class="download-information">
+                250Mb / 5Gb (2.45Mbs) - 25%
+            </div>
+            <div class="download-progress">
+                <progress class="download-actual-progress" style="display:none" value=".0"></progress>
+                <div class="download-progress-bar">
+                    <div class="download-progress-done">
+                    </div>
+                </div>
+            </div>
+            <div class="download-open-folder">
+
+            </div>
+            <div class="download-delete">
+
+            </div>
+    </div>
+    `;
+    }
+
 }
